fee68d6e951cfe1f676c7bb11605a83ccc85636f
locklamber <huamanvelasquez.antony@gmail.com>
2023-03-17 09:39:22 +0100

    inicializamos ejemplo2

--- ejemplo2.java
+++ ejemplo2.java
@@ -1,5 +1,23 @@
 package trabajo;
 
+import java.util.*;
+
 public class ejemplo2 {
+	
+	Scanner teclado = new Scanner(System.in);
+	
+	private double lado1, lado2, lado3;
+
+	public void inicializar() {
+	
+		System.out.println("Ingrese el primer lado del trangulo:");
+		lado1=teclado.nextInt();
+
+		System.out.println("Ingrese el segundo lado del triangulo:");
+		lado2=teclado.nextInt();
+
+		System.out.println("Ingrese el tercer lado del triangulo:");
+		lado3=teclado.nextInt();
+	}
 
 }
