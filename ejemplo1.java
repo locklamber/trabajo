edfb2277c2b5d624b9e1d6a0dbc3b6284ca9a18a
locklamber <huamanvelasquez.antony@gmail.com>
2023-03-17 09:31:36 +0100

    inicializamos ejemplo1

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,5 +1,18 @@
 package trabajo;
 
+import java.util.*;
+
 public class ejemplo1 {
 
+	private double lado, perimetro, area ;
+
+	public void inicializar() {
+	
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Ingrese el lado del cuadrado :");
+		lado=teclado.nextInt();
+
+	}
+	
 }
