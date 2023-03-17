942417da6a74c32d93ff56cb0b8286c8ded10376
locklamber <huamanvelasquez.antony@gmail.com>
2023-03-17 09:43:54 +0100

    inicializamos ejemplo3

--- ejemplo3.java
+++ ejemplo3.java
@@ -1,5 +1,21 @@
 package trabajo;
 
+import java.util.*;
+
 public class ejemplo3 {
+	
+	Scanner teclado = new Scanner(System.in);
+	
+	private int x, y;
+
+	public void inicializar() {
+	
+		System.out.println("Ingrese x :");
+		x=teclado.nextInt();
+
+		System.out.println("Ingrese y :");
+		y=teclado.nextInt();
+
+	}
 
 }
